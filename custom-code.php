6254969e4c2962195e49dec7681e94b5efbf819e
BP <bp@BPs-MacBook-Air.local>
2026-05-05 12:31:07 +0530

    Add Google Analytics tag and clean up header meta tags

--- custom-code.php
+++ custom-code.php
@@ -1,2 +1,12 @@
 <!-- Custom Code Injected in Head -->
 <meta name="google-site-verification" content="68gERuOjtmX4ai5qH8foc1K5VnPkurMVPF6avntlnLc" />
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EETLNH2YGP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EETLNH2YGP');
+</script>
--- header.php
+++ header.php
@@ -99,5 +99,3 @@
 
 <body class="bg-gray-50">
     <?php include($base_url . 'components/navbar.php'); ?>
-
-    <meta name="google-site-verification" content="68gERuOjtmX4ai5qH8foc1K5VnPkurMVPF6avntlnLc" />
